67a16a2a5a8ecd79b73b72f5c0152fa6b2118151
Zigzagzen7 <alexandre.marcil@gmail.com>
2019-04-19 23:00:27 -0400

    code cleanup

--- dataloader.py
+++ dataloader.py
@@ -1,37 +1,25 @@
 import glob
+
 import pandas as pd
-import json as j
-import pandas as pd
-import re
-import numpy as np
-from nltk.corpus import stopwords
-from nltk.stem import SnowballStemmer
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.svm import LinearSVC
-from sklearn.pipeline import Pipeline
-from sklearn.model_selection import train_test_split
-from sklearn.feature_selection import SelectKBest, chi2
 
 
-def getReviews(file):
-    f=open(file)
-    lines=f.readlines()
+def getreviews(file):
+    f = open(file)
+    lines = f.readlines()
     f.close()
-    reviews=[]
+    reviews = []
     for l in lines:
-        x=l.split('\t')
-        reviews.append([str.lstrip(str.rstrip(x[0])),str.lstrip(str.rstrip(x[1]))])
+        x = l.split('\t')
+        reviews.append([str.lstrip(str.rstrip(x[0])), str.lstrip(str.rstrip(x[1]))])
     return reviews
 
 
 def dataload(path):
-    reviewsList = []
+    reviews_list = []
     for files in glob.glob(path + "/*.txt"):
-        reviewsList += (getReviews(files))
-    df =  pd.DataFrame(reviewsList, columns=['review', 'label'])
+        reviews_list += (getreviews(files))
+    df = pd.DataFrame(reviews_list, columns=['review', 'label'])
+    df['label'] = df['label'].astype(int)  # for classification report
 
-    df['label']= df['label'].astype(int)
-
-    # print(df['label'][0])
     return df
 
--- train.py
+++ train.py
@@ -1,4 +1,13 @@
+'''
+    Alexandre Marcil
+    18 avril 2019
+    inspired from https://scikit-learn.org/stable/tutorial/text_analytics/working_with_text_data.html
+    dataset from https://archive.ics.uci.edu/ml/datasets/Sentiment+Labelled+Sentences
+'''
+
+
 import warnings
+import pickle
 
 from sklearn.model_selection import train_test_split
 
@@ -6,49 +15,42 @@ from dataloader import dataload
 from models import SVM, NaiveBayes, RandomForest, LogisticRegression, MLP
 
 PATH = 'data/'
-SEED = 4
+SEED = 108
+SPLIT = 0.8
 
 if __name__ == "__main__":
-    # warnings.filterwarnings("ignore", module = r'.*sklearn')
+
     warnings.simplefilter(action='ignore', category=FutureWarning)
 
     data = dataload(PATH)
 
     X_train, X_test, y_train, y_test = train_test_split(data['review'], data['label'], stratify=data['label'],
-                                                        test_size=0.2, random_state=SEED)
+                                                        train_size=SPLIT, random_state=SEED)
 
-    # Multi-Layer Perceptron
-    mlp = MLP(X_train, y_train)
-    mlp.evaluate(X_test, y_test)
-
+    print("Training models and printing training accuracies...")
     # Random Forest
     rfc = RandomForest(X_train, y_train)
-    rfc.evaluate(X_test, y_test)
-
-    # Logistic Regression
-    lr = LogisticRegression(X_train, y_train)
-    lr.evaluate(X_test, y_test)
+    rfc.evaluate(X_train, y_train)
 
     # Naive Bayes Classifier
     nbc = NaiveBayes(X_train, y_train)
-    nbc.evaluate(X_test, y_test)
+    nbc.evaluate(X_train, y_train)
 
     # SVM
     svm = SVM(X_train, y_train)
-    svm.evaluate(X_test, y_test)
+    svm.evaluate(X_train, y_train)
 
-    # Save Naive Bayes Model
-    # nb_pickle = open(config.naive_bayes_path, 'wb')
-    # pickle.dump(nb_model_nb, nb_pickle)
-    # nb_pickle.close()
+    # Logistic Regression
+    lrc = LogisticRegression(X_train, y_train)
+    lrc.evaluate(X_train, y_train)
 
-    # Save SVM Model
-    # svm_pickle = open(config.SVM_path, 'wb')
-    # pickle.dump(nb_model_nb, svm_pickle)
-    # svm_pickle.close()
+    # Multi-Layer Perceptron
+    mlp = MLP(X_train, y_train)
+    mlp.evaluate(X_train, y_train)
 
-    # nb_model_rfc.evaluate(X_test, y_test)
-    # predicted = nb_model_rfc.predict(X_test)
-    # print('results nb', nb_model_nb.loss_fct(predicted, y_test))
+    # Saving our models as a dictionary with 5 components
+    saved_model = open("models" + ".pkl", "wb")
+    pickle.dump({0: SEED, 1: rfc, 2: nbc, 3: svm, 4: lrc, 5: mlp}, saved_model)
+    saved_model.close()
 
-    # print('results nb', nb_model_nb.loss_fct(predicted, y_test))
+
